7748d0485e6343eaf55f29a3f049e62704bb797a
Sebastian Amann <sebastian.amann92@gmail.com>
2022-08-12 17:05:43 +0200

    feature: add calculation Unit Test

--- Library.NUnit/PositionFixture.cs
+++ Library.NUnit/PositionFixture.cs
@@ -1,17 +1,20 @@
 using NUnit.Framework;
+using Library;
 
 namespace Library.NUnit
 {
     internal sealed class PositionFixture
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
+        private Position pOne = new Position(120, 60, 5);
+        private Position pTwo = new Position(130, 70, 5);
+        private Position pThree = new Position(150, 89, 5);
+        private Position pFour = new Position(140, 45, 5);
 
         [Test]
-        public void Test()
+        public void CalculatinDistance()
         {
+            var distance = PolygonCalculatorHelper.CalculateDistance(pOne, pTwo);
+            Assert.AreEqual(11876156.017462872d, distance);
         }
 
     }
